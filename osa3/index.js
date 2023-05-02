56692f7d7ac98edfab90d9a86cbdeac3680526aa
Tjkallio1 <tuomask32@gmail.com>
2023-05-02 16:32:02 +0300

    Tiistain vikat

--- osa3/index.js
+++ osa3/index.js
@@ -1,15 +1,30 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan') 
 const cors = require('cors')
+const Person = require('./models/person')
 
 const app = express()
+
+const currentDate = Date()
+
+const errorHandler = (error, request, response, next) => {
+    console.log(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+    next(error)
+}
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
 
 app.use(express.json())
 app.use(morgan('combined'))
 app.use(cors())
 app.use(express.static('build'))
-
-const currentDate = Date()
 
 function getRandomId(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min
@@ -25,7 +40,7 @@ morgan(function (tokens, req, res) {
     ].join(' ')
   })
 
-
+/*
 let persons = [
     {
         name: "Arto Hellas",
@@ -58,21 +73,39 @@ let persons = [
         id: "231a87b6-a7ae-4b14-8cce-a21c0c8d952b"
         }
 ]
+*/
 
 app.get('/', (req, res) => {
     res.send('<h1>Hello World</h1>')
 })
 
 app.get('/info', (req, res) => {
-    res.send(`<p>Phonebook has info for ${names.length} people</p><p>${currentDate}</p>`)
-
-})
+    Person.countDocuments({})
+        .then(count => {
+            res.send(`<p>Phonebook has info for ${count} people</p><p>${currentDate}</p>`)
+        })         
+    } )
+    
 
 app.get('/api/persons', (req, res) => {
-    res.json(persons)
+    Person.find({}).then(persons => {
+        res.json(persons)
+    })
 })
 
 app.get('/api/persons/:id', (request, response) => {
+    Person.findById(request.params.id)
+        .then(person => {
+        if(person) {
+            response.json(person)
+        } else {
+            response.status(404).end()
+        }
+    })
+    .catch(error => next(error))
+})
+
+    /*
     const id = Number(request.params.id)
     const person = persons.find(person => person.id === id)
     
@@ -81,7 +114,7 @@ app.get('/api/persons/:id', (request, response) => {
     } else {
         response.status(404).end()
     }
-})
+    */
 
 app.post('/api/persons', (request, response) => {
     const newId = getRandomId(10,150)
@@ -93,31 +126,54 @@ app.post('/api/persons', (request, response) => {
         })
     }
 
+    /*
     const existingPerson = persons.find(person => person.name === body.name)
     if(existingPerson) {
         return response.status(400).json ({
             error: 'name must be unique'
         })
     }
+    */
     
-    const person = {
+    const person = new Person ({
         name: body.name,
         number: body.number,
         id: newId
+    })
+    person.save().then(savedPerson =>{
+        response.json(savedPerson)
+    })
+})
+
+app.put('/api/persons/:id', (request, response, next) => {
+    const body = request.body
+
+    const person = {
+        name: body.name,
+        number: body.number
     }
-    persons.push(person)
-    console.log(person)
-    response.json(person)
+
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+        .then(updatedPerson => {
+            response.json(updatedPerson)
+        })
+        .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-
-    response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+    //const id = Number(request.params.id)
+    //persons = persons.filter(person => person.id !== id)
+    Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+        response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
-const PORT = process.env.PORT || 3001
+app.use(unknownEndpoint)
+app.use(errorHandler)
+
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`)
 })
