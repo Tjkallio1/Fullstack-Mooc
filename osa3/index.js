ecaaf6b4b39d84330de97d4ee1155c96db053889
Tjkallio1 <tuomask32@gmail.com>
2023-05-05 15:48:36 +0300

    Osa3 viimeistely

--- osa3/index.js
+++ osa3/index.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const express = require('express')
-const morgan = require('morgan') 
+const morgan = require('morgan')
 const cors = require('cors')
 const Person = require('./models/person')
 
@@ -8,19 +8,20 @@ const app = express()
 
 const currentDate = Date()
 
+// eslint-disable-next-line consistent-return
 const errorHandler = (error, request, response, next) => {
-    console.log(error.message)
+  console.log(error.message)
 
-    if (error.name === 'CastError') {
-        return response.status(400).send({ error: 'malformatted id' })
-    } else if (error.name === 'ValidationError') {
-        return response.status(400).json({ error: error.message })
-    }
-    next(error)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+  next(error)
 }
 
 const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
+  response.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(express.json())
@@ -28,19 +29,19 @@ app.use(morgan('combined'))
 app.use(cors())
 app.use(express.static('build'))
 
+/*
 function getRandomId(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min
+  return Math.floor(Math.random() * (max - min + 1)) + min
 }
+*/
 
-morgan(function (tokens, req, res) {
-    return [
-      tokens.method(req, res),
-      tokens.url(req, res),
-      tokens.status(req, res),
-      tokens.res(req, res, 'content-length'), '-',
-      tokens['response-time'](req, res), 'ms'
-    ].join(' ')
-  })
+morgan((tokens, req, res) => [
+  tokens.method(req, res),
+  tokens.url(req, res),
+  tokens.status(req, res),
+  tokens.res(req, res, 'content-length'), '-',
+  tokens['response-time'](req, res), 'ms',
+].join(' '))
 
 /*
 let persons = [
@@ -78,39 +79,38 @@ let persons = [
 */
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World</h1>')
+  res.send('<h1>Hello World</h1>')
 })
 
 app.get('/info', (req, res) => {
-    Person.countDocuments({})
-        .then(count => {
-            res.send(`<p>Phonebook has info for ${count} people</p><p>${currentDate}</p>`)
-        })         
-    } )
-    
+  Person.countDocuments({})
+    .then((count) => {
+      res.send(`<p>Phonebook has info for ${count} people</p><p>${currentDate}</p>`)
+    })
+})
 
 app.get('/api/persons', (req, res) => {
-    Person.find({}).then(persons => {
-        res.json(persons)
-    })
+  Person.find({}).then((persons) => {
+    res.json(persons)
+  })
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
-    Person.findById(request.params.id)
-        .then(person => {
-        if(person) {
-            response.json(person)
-        } else {
-            response.status(404).end()
-        }
+  Person.findById(request.params.id)
+    .then((person) => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
     })
-    .catch(error => next(error))
+    .catch((error) => next(error))
 })
 
-    /*
+/*
     const id = Number(request.params.id)
     const person = persons.find(person => person.id === id)
-    
+
     if(person) {
         response.json(person)
     } else {
@@ -119,22 +119,22 @@ app.get('/api/persons/:id', (request, response, next) => {
     */
 
 app.post('/api/persons', (request, response, next) => {
-    //const newId = getRandomId(10,150)
-    const body = request.body
-    
-    const person = new Person ({
-        name: body.name,
-        number: body.number
-    })
+  // const newId = getRandomId(10,150)
+  const { body } = request
 
-    person.save()
-        .then(savedPerson => {
-            response.json(savedPerson)
-        })
-        .catch(error => next(error))
+  const person = new Person({
+    name: body.name,
+    number: body.number,
+  })
+
+  person.save()
+    .then((savedPerson) => {
+      response.json(savedPerson)
     })
-    
-    /*
+    .catch((error) => next(error))
+})
+
+/*
     if(!body.name || !body.number) {
         return response.status(400).json ({
             error: 'name or number missing'
@@ -142,7 +142,7 @@ app.post('/api/persons', (request, response, next) => {
     }
     */
 
-    /*
+/*
     const existingPerson = persons.find(person => person.name === body.name)
     if(existingPerson) {
         return response.status(400).json ({
@@ -152,37 +152,40 @@ app.post('/api/persons', (request, response, next) => {
     */
 
 app.put('/api/persons/:id', (request, response, next) => {
-    const { name, number } = request.body
+  const { name, number } = request.body
 
-    /*
+  /*
     const person = {
         name: body.name,
         number: body.number
     */
 
-    Person.findByIdAndUpdate(request.params.id, 
-        {name, number}, 
-        { new: true, runValidators: true })
-        .then(updatedPerson => {
-            response.json(updatedPerson)
-        })
-        .catch(error => next(error))
+  Person.findByIdAndUpdate(
+    request.params.id,
+    { name, number },
+    { new: true, runValidators: true },
+  )
+    .then((updatedPerson) => {
+      response.json(updatedPerson)
+    })
+    .catch((error) => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response, next) => {
-    //const id = Number(request.params.id)
-    //persons = persons.filter(person => person.id !== id)
-    Person.findByIdAndRemove(request.params.id)
-    .then(result => {
-        response.status(204).end()
+  // const id = Number(request.params.id)
+  // persons = persons.filter(person => person.id !== id)
+  Person.findByIdAndRemove(request.params.id)
+    // eslint-disable-next-line no-unused-vars
+    .then((result) => {
+      response.status(204).end()
     })
-    .catch(error => next(error))
+    .catch((error) => next(error))
 })
 
 app.use(unknownEndpoint)
 app.use(errorHandler)
 
-const PORT = process.env.PORT
+const { PORT } = process.env
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
--- osa3/models/person.js
+++ osa3/models/person.js
@@ -1,31 +1,38 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-unused-vars */
+/* eslint-disable linebreak-style */
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable linebreak-style */
 const mongoose = require('mongoose')
 
-mongoose.set('strictQuery',false)
+mongoose.set('strictQuery', false)
 
 const url = process.env.MONGODB_URI
 console.log('connecting to', url)
 mongoose.connect(url)
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch((error) => {
-        console.log('error connecting to MongoDB:', error.message)
-    })
+  .then((result) => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minlength: 3
-    } ,
-    number: String,
+  name: {
+    type: String,
+    minlength: 3,
+  },
+  number: String,
 })
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    // eslint-disable-next-line no-underscore-dangle
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  },
 })
 
 module.exports = mongoose.model('Person', personSchema)
