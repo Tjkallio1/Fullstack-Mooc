85976eb2dec8b328e9f99dbc926148aa4a70142c
Tjkallio1 <tuomask32@gmail.com>
2023-05-11 15:54:22 +0300

    Testausta jatkettu

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -6,8 +6,18 @@ const dummy = (blogs) => {
     const likes = blogs.reduce((sum, blog) => sum + blog.likes, 0)
     return likes
   }
+
+  const mostLikes = (blogs) => {
+    const sorted = blogs.sort((a, b) => b.likes - a.likes)
+    const mostLiked = sorted[0]
+    
+    const { title, author, likes } = mostLiked
+    const mostLikedObject = JSON.stringify({ title, author, likes })
+    return mostLikedObject
+  }
   
   module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    mostLikes
   }
--- osa4/utils/list_helper.test.js
+++ osa4/utils/list_helper.test.js
@@ -37,3 +37,37 @@ test('total likes counted', () => {
   expect(likeResults).toBe(40)
 })
 })
+
+describe('blog with most likes', () => {
+  const blogs = [
+    {
+    title: "Esimerkkiblogi1",
+    author: "Esimerkki Eetu",
+    url: "http://www.blogitestaus.fi",
+    likes: 7,
+    __v: 0
+    },
+    {
+    title: "Esimerkkiblogi2",
+    author: "Esimerkki Erkki",
+    url: "http://www.skeidaa.fi",
+    likes: 12,
+    __v: 0
+    },
+    {
+    title: "Esimerkkiblogi3",
+    author: "Esimerkki Eemeli",
+    url: "http://www.juuh.fi",
+    likes: 21,
+    __v: 0
+    }
+    ]
+
+test('most likes', () => {
+  const mostResult = listHelper.mostLikes(blogs)
+  const expectedBlog = {"title": "Esimerkkiblogi3", "author": "Esimerkki Eemeli",
+  "likes": 21}
+  const parsed = JSON.parse(mostResult)
+  expect(parsed).toEqual(expectedBlog)
+  })
+})
