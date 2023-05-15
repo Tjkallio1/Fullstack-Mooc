0b662567e720551af3a6a2ac41b80a2fc9522c31
Tjkallio1 <tuomask32@gmail.com>
2023-05-15 18:48:45 +0300

    Testailua jatkettu

--- osa4/controllers/blogs.js
+++ osa4/controllers/blogs.js
@@ -1,23 +1,62 @@
 const blogsRouter = require('express').Router()
+const { request, response } = require('../app')
 const Blog = require('../models/blog')
 
-blogsRouter.get('/blogs', (request, response) => {
+blogsRouter.get('/blogs', async (request, response) => {
     console.log(request.body)
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-blogsRouter.post('/blogs', (request, response) => {
-    const blog = new Blog(request.body)
-  
-    blog
-      .save()
-      .then(result => {
-        response.status(201).json(result)
-      })
+    const blogs = await Blog.find({})
+        response.json(blogs)
   })
 
-  module.exports = blogsRouter
+blogsRouter.get('/blogs/:id', async (request, response) => {
+    console.log('get by ID route hit')
+    const blog = await Blog.findById(request.params.id)
+    console.log('blog', blog)
+    if(blog) {
+      response.json(blog)
+    } else {
+      response.status(404).end()
+    }
+})
+
+blogsRouter.post('/blogs', async (request, response, next) => {   
+  const body = request.body
+
+  if(!body.title || !body.url) {
+    return response.status(400).json({error: 'title or url missing'})
+  }
+ 
+  const blog = new Blog ({
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes || 0 
+  })
+  const savedBlog = await blog.save()
+  response.status(201).json(savedBlog)
+})
+
+blogsRouter.delete('/blogs/:id', async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id) 
+  response.status(204).end()
+  })
+ 
+
+blogsRouter.put('/blogs/:id', (request, response, next) => {
+    const body = request.body
+
+    const blog = {
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes
+    }
+  
+  Blog.findByIdAndUpdate(request.params.id, blog,  { new: true })
+   .then(updatedBlog => {
+      response.json(updatedBlog)
+   }) 
+  .catch(error  => next(error))
+})
+
+module.exports = blogsRouter
--- osa4/models/blog.js
+++ osa4/models/blog.js
@@ -8,12 +8,12 @@ const blogSchema = new mongoose.Schema({
   })
 
   
-  blogSchema.set('toJson', {
+  blogSchema.set('toJSON', {
     transform: (document, returnedObject) => {
         returnedObject.id = returnedObject._id.toString()
         delete returnedObject._id
         delete returnedObject.__v
-    }
+      }
   })
   
 
