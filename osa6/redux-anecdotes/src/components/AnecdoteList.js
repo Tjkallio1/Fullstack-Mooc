d3787b8faf007ab6c77b7c663dc5623d92d408a6
Tjkallio1 <tuomask32@gmail.com>
2023-06-28 16:51:02 +0300

    6 osaa jatkettu

--- osa6/redux-anecdotes/src/components/AnecdoteList.js
+++ osa6/redux-anecdotes/src/components/AnecdoteList.js
@@ -1,9 +1,12 @@
+import React, { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { addVote } from "../reducers/anecdoteReducer"
+import Filter from "./Filter"
 
 const AnecdoteList = () => {
 
-  const anecdotes = useSelector(state => state)
+  const anecdotes = useSelector(state => state.anecdotes)
+  const filter = useSelector(state => state.filter)
   const dispatch = useDispatch()
 
   const vote = (id) => {
@@ -11,12 +14,19 @@ const AnecdoteList = () => {
     console.log('vote', id)
   }
 
-  const sortedAnecdotes = [...anecdotes].sort((a, b) => b.votes - a.votes)
+  const filteredAnecdotes = anecdotes.filter((anecdote) => 
+  anecdote.content.toLowerCase().includes(filter.toLowerCase())
+  )
+
+  const sortedAnecdotes = filteredAnecdotes.sort((a, b) => b.votes - a.votes)
+
+  const displayedAnecdotes = filter === 'ALL' ? anecdotes : sortedAnecdotes
 
   return (
     <div>
       <h2>Anecdotes</h2>
-      {sortedAnecdotes.map(anecdote => (
+      <Filter />
+      {displayedAnecdotes.map(anecdote => (
         <div key={anecdote.id}>
           <div>
             {anecdote.content}
