42d7a7ed862dbf28ca7b39a08eb8b5c668e1df99
Tjkallio1 <tuomask32@gmail.com>
2023-06-30 17:16:57 +0300

    Osa 6.12. tehty ja 13 osittain

--- osa6/redux-anecdotes/src/components/AnecdoteList.js
+++ osa6/redux-anecdotes/src/components/AnecdoteList.js
@@ -2,6 +2,8 @@ import React from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { addVote } from "../reducers/anecdoteReducer"
 import Filter from "./Filter"
+import Notification from "./Notification"
+import { clearNotification, setNotification } from "../reducers/notificationReducer"
 
 const AnecdoteList = () => {
 
@@ -9,9 +11,14 @@ const AnecdoteList = () => {
   const filterValue = useSelector(state => state.filter)
   const dispatch = useDispatch()
 
-  const vote = (id) => {
+  const vote = (id, content) => {
     dispatch(addVote( { id }))
     console.log('vote', id)
+    dispatch(setNotification('you voted', {content})) //tänne pitää saada äänestetty anekdootti
+    console.log('content', content)
+    setTimeout(() => {
+      dispatch(clearNotification())
+    }, 5000)
   }
 
   const sortedAnecdotes = [...anecdotes].sort((a, b) => b.votes - a.votes)
@@ -24,10 +31,12 @@ const AnecdoteList = () => {
 
   const displayedAnecdotes = filterValue === 'ALL' ? sortedAnecdotes : filteredAnecdotes
 
+  // notification pitää saada häviämään perustilassa ja 5 sekuntia äänestämisestä 
   return (
     <div>
       <h2>Anecdotes</h2>
-      <Filter />
+      <Notification />
+      <Filter /> 
       {displayedAnecdotes.map(anecdote => (
         <div key={anecdote.id}>
           <div>
