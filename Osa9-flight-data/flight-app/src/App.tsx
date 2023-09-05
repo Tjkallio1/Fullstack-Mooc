83446dff163ca96bc00b6bbc635ad9ae0e60fd3d
Tjkallio1 <tuomask32@gmail.com>
2023-09-05 16:09:08 +0300

    Osa 9.17 ja 9.18 tehty

--- Osa9-flight-data/flight-app/src/App.tsx
+++ Osa9-flight-data/flight-app/src/App.tsx
@@ -1,23 +1,94 @@
 import { useState, useEffect } from 'react';
 import { Diary } from './types';
-import axios from 'axios';
+import { getAllDiaries, createDiary } from './diaryService';
 
 const App = () => {
 
   const [diaries, setDiaries] = useState<Diary[]>([]);
+  const [date, setDate] = useState('');
+  const [visibility, setVisibility] = useState('');
+  const [weather, setWeather] = useState('');
+  const [comment, setComment] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [errorVisible, setErrorVisible] = useState(false);
 
   useEffect(() => {
-    axios.get<Diary[]>('http://localhost:3001/api/diaries').then(response => {
-      setDiaries(response.data)
+    getAllDiaries().then(data => {
+      setDiaries(data)
+      console.log('diaries listed')
     })
   }, [])
+
+  const showError = (message: string, duration: number) => {
+    setErrorMessage(message);
+    setErrorVisible(true);
+    setTimeout(() => {
+      setErrorVisible(false);
+      setErrorMessage('');
+    }, duration);
+  };
+
+  const diaryCreation =(event: React.SyntheticEvent) => {
+    event.preventDefault()
+    createDiary({
+      date: date,
+      visibility: visibility,
+      weather: weather,
+      comment: comment
+    })
+    .then((data) => {
+      if (typeof data === 'string') {
+        showError(data, 5000);
+      } else {
+        setDiaries((prevDiaries) => [...prevDiaries, data]);
+        console.log('added diary:', data);
+      }
+    })
+    .catch((error) => {
+      console.error('Error creating diary:', error);
+    });
+
+    setDate('')
+    setVisibility('')
+    setWeather('')
+    setComment('')
+  };
 
   return (
     <div>
       <h2>Add new entry</h2>
+      <div className={errorVisible ? 'error-message' : 'hidden-error-message'}>
+          {errorMessage}
+      </div>
+      <div>
+        <form onSubmit={diaryCreation}>
+          <div>
+            date
+            <input value={date} onChange={(event) => setDate(event.target.value)} />
+          </div>
+          <div>
+            visibility
+            <input value={visibility} onChange={(event) => setVisibility(event.target.value)} />
+          </div>
+          <div>
+            weather
+            <input value={weather} onChange={(event) => setWeather(event.target.value)} />
+          </div>
+          <div>
+            comment
+            <input value={comment} onChange={(event) => setComment(event.target.value)} />
+          </div>
+          <button type='submit'>add</button>
+        </form>
+      </div>
       <h2>Diary entries</h2>
       {diaries.map(diary =>
-          <p key={diary.id}>{diary.date} {diary.weather} {diary.visibility}</p>
+          <div key={diary.id}>
+            <p><strong>{diary.date}</strong></p> 
+            <p>weather: {diary.weather}</p> 
+            <p>visibility: {diary.visibility}</p> 
+            <p>comment: {diary.comment}</p>         
+          </div>
         )}
     </div>
   );
---
+++ Osa9-flight-data/flight-app/src/diaryService.tsx
@@ -0,0 +1,46 @@
+import axios, { AxiosError } from 'axios'
+import { Diary, NewDiary } from './types'
+
+interface ValidationError {
+    message: string; 
+    errors: Record<string, string[]>
+  }
+
+const baseurl = 'http://localhost:3001/api/diaries'
+
+export const getAllDiaries = () => {
+    return axios
+        .get<Diary[]>(baseurl)
+        .then(response => response.data)
+}
+
+export const createDiary = async (newDiary: NewDiary): Promise<Diary | string> => {
+
+    try {
+      const response = await axios.post<Diary>(baseurl, newDiary);
+      console.log(response.data);
+      return response.data;
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+          const axiosError: AxiosError<ValidationError, Record<string, unknown>> = error;
+          const status = axiosError.response?.status;
+          const responseData = axiosError.response?.data;
+    
+          if (status === 400) {
+            if (responseData && responseData.message) {
+              return `Error: ${responseData.message}`;
+            } else {
+              return 'Error: Incorrect data';
+            }
+          } else if (status === 401) {
+            if (responseData && responseData.message) {
+              return `Unauthorized: ${responseData.message}`;
+            } else {
+              return 'Unauthorized';
+            }
+          }
+        }
+        console.error(error);
+        return 'An unknown error occurred'; 
+      }
+  };
