a0dcd5f76e7a6ed57de65b979f1187275d19a7cd
Tjkallio1 <tuomask32@gmail.com>
2023-09-07 15:42:16 +0300

    9.22 tehty

--- Patientor-backend/src/types.ts
+++ Patientor-backend/src/types.ts
@@ -4,9 +4,47 @@ export interface DiagnoseForm {
     latin?: string
 }
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {
-}
+interface BaseEntry {
+    id: string;
+    description: string;
+    date: string;
+    specialist: string;
+    diagnosisCodes?: Array<DiagnoseForm['code']>;
+  }
+
+  export enum HealthCheckRating {
+    "Healthy" = 0,
+    "LowRisk" = 1,
+    "HighRisk" = 2,
+    "CriticalRisk" = 3
+  }
+
+  interface HospitalEntry extends BaseEntry {
+    type: "Hospital";
+    discharge: {
+        date: string,
+        criteria: string,
+    }
+  }
+  
+  interface OccupationalHealthcareEntry extends BaseEntry {
+    type: "OccupationalHealthcare";
+    employerName: string,
+    sickLeave?: {
+        startDate: string,
+        endDate: string,
+      }
+  }
+
+  interface HealthCheckEntry extends BaseEntry {
+    type: "HealthCheck";
+    healthCheckRating: HealthCheckRating;
+  }
+
+  export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
 
 export interface PatientForm {
     id: string,
